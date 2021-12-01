89c35a93e7f0a1b8656e163f35200880b346fcf5
samkeliso <samchiota@gmail.com>
2021-12-01 20:33:12 +0000

    added session 4 javascript tasks

--- session-4/index.js
+++ session-4/index.js
@@ -1,4 +1,34 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+let personA = {
+    name: 'Leonardo',
+age: '16',
+  location: 'New York City', 
+  likes: 'blue'
+};
+let personB = {
+    name: 'Michaelangelo',
+age: '16',
+ location: 'New York City', 
+ likes: 'orange'
+};
+let personC = {
+    name: 'Raphael',
+age: '16',
+ location: 'New York City', 
+ likes: 'red'
+};
+let personD = {
+    name: 'Donatello',
+age: '16',
+ location: 'New York City', 
+ likes: 'purple'
+};
+
+function biography (person) {
+    return 'Hi, my name is ' + person.name + ' I live in ' + person.location + ' and my bandana is ' + person.likes
+
+};
+console.log(biography (personC));
 
 
 
@@ -19,24 +49,38 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+ const x = 6;
+ const y = 4;
 
-// const addition = x + y;
+ const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+ console.log("Addition: x + y = " + addition);
 
-// const subtraction = x - y;
+ const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+ console.log("Subtraction: x - y = " + subtraction);
 
-// const multiplication = x * y;
+ const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+ console.log("multiplication: x * y = " + multiplication);
 
-// const division = x * y;
+ const division = x * y;
 
-// console.log("Division: x + y " + division);
+ console.log("Division: x + y = " + division);
+
+ const times = x * x 
+
+ console.log("times: x * x = " + times)
+
+ const plus  = x + x + x
+
+ console.log('plus: x + x + x = ' + plus)
+ 
+ const fraction = x * y / x
+
+ console.log('fraction: x * y / x = ' + fraction )
+
+ 
 
 
 
@@ -54,6 +98,12 @@
 
 // ****************************************************TASK 3****************************************************
 
+
+const myAge = 24;
+const drivingAge = 18;
+
+const isMyAgeGreater = (myAge > drivingAge);
+console.log("Am I old enough to drive? " + isMyAgeGreater)
 
 
 
