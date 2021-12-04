e4b0781573966f92305153e486808e7733fcfee3
samkeliso <samchiota@gmail.com>
2021-12-04 23:53:32 +0000

    added session 2 tasks

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,27 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+// **************************************TASK 1*********************************************************
+console.log("hello everyone")
+
+// INTERACTING WITH THE DOM
+
+console.log(document.querySelector('.about').innerHTML);
+console.log(document.querySelectorAll('.about'))
+
+// Learning functions
+
+function sayHey () {
+    console.log('How are you?');
+    console.log('Goodbye');
+}
+sayHey();
+
+function conversation () {
+    sayHey();
+    console.log('How are you?');
+    console.log('Goodbye');
+}
+conversation();
+
+
