e93f572110b37a065a79d1738163c75fa9978c6c
samkeliso <samchiota@gmail.com>
2021-12-06 20:03:13 +0000

    fixed code for Task 1 and added Task 2

--- session-5/index.js
+++ session-5/index.js
@@ -1,31 +1,31 @@
-// TASK 1
+TASK 1
 function getGrade (total) { 
     if (total > 90) {
         console.log('You got an A+');
     }
    
-    if (total > 80, total <= 90) {
+    if (total > 80 && total <= 90) {
         console.log('You got an A') 
     }
 
-    if (total  > 70, total <= 80) {
+    if (total  > 70 && total <= 80) {
         console.log('You got a B')
     }
 
-    if (total > 60, total <= 70){
+    if (total > 60 && total <= 70){
         console.log ('You got a C')
     }
 
-    if (total > 50, total <= 60){
+    if (total > 50 && total <= 60){
         console.log ('You got a D')
     }
 
-    if (total > 40, total <= 50){
-        console.log ('You got a E')
+    if (total > 40 && total <= 50){
+        console.log ('You got an E')
     }
 
-    if (total > 30, total <= 40){
-        console.log ('You got a F')
+    if (total > 30 && total <= 40){
+        console.log ('You got an F')
     }
 
     if (total < 30)  {
@@ -36,8 +36,53 @@ function getGrade (total) {
 };
 
 console.log(getGrade(85));
-// console.log(getGrade(67));
-// console.log(getGrade(53));
+console.log(getGrade(67));
+console.log(getGrade(53));
+
+
+TASK 2 
+
+function getGrade (total) {
+    switch (true) {
+        case total > 90:
+        console.log('You got an A+');
+        break;
+
+        case total > 80 && total <= 90:
+        console.log('You got an A');
+        break;
+
+        case total > 70 && total <= 80:
+        console.log('You got a B');
+        break;
+
+        case total > 60 && total <= 70:
+        console.log('You got a C');
+        break;
+    
+
+         case total > 50 && total <= 60:
+        console.log('You got a D');
+        break;
+
+        case total > 40 && total <=50:
+        console.log('You got an E');
+        break;
+
+        case total > 30 && total <= 40:
+        console.log('You got an F');
+        break;
+
+        case total > 30:
+        console.log('YOU FAILED');
+        break;
+      
+};
+
+}
+
+console.log(getGrade(47));
+console.log(getGrade(58));
 
 
 
