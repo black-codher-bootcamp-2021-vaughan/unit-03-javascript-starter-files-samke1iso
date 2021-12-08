8649cc3eb20d00a741147d96195d7449f2ea82ad
samkeliso <samchiota@gmail.com>
2021-12-08 18:41:39 +0000

    added Task 1(Loops)

--- session-6/index.js
+++ session-6/index.js
@@ -1,12 +1,18 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
-const randomNumber = Math.round(Math.random() * 10);
+// const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
 
+let count = 1
+const randomNumber = Math.round(Math.random() * 10);
 
+while (count < 10) {
+    const randomNumber = Math.round(Math.random() * 10);
+    console.log(randomNumber);
+    count++;
+};
 
-
-
+console.log(randomNumber);
 
 
 
